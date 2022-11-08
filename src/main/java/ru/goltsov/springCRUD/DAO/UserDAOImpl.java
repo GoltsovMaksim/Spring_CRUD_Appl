ff83ae8750569281b93c6923d48f1034a7be2ee0
Maksim Goltsov <mgoltsov@bk.ru>
2022-11-09 08:40:14 +1200

    controller and views added

--- src/main/java/ru/goltsov/springCRUD/DAO/UserDAOImpl.java
+++ src/main/java/ru/goltsov/springCRUD/DAO/UserDAOImpl.java
@@ -5,30 +5,36 @@ import ru.goltsov.springCRUD.models.User;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Component
 public class UserDAOImpl implements UserDAO {
     private static int USER_COUNT;
-    private List<User> listUser = new ArrayList<>();
+    private final List<User> listUsers = new ArrayList<>();
+
     {
-        listUser.add(new User(++USER_COUNT,"Aser","Qwerty"));
-        listUser.add(new User(++USER_COUNT,"Bob","Tompson"));
-        listUser.add(new User(++USER_COUNT,"Charles","Loly"));
-        listUser.add(new User(++USER_COUNT,"Doka","Carl"));
-        listUser.add(new User(++USER_COUNT,"Elena","Ivanova"));
+        listUsers.add(new User(++USER_COUNT, "Aser", "Qwerty"));
+        listUsers.add(new User(++USER_COUNT, "Bob", "Tompson"));
+        listUsers.add(new User(++USER_COUNT, "Charles", "Loly"));
+        listUsers.add(new User(++USER_COUNT, "Doka", "Carl"));
+        listUsers.add(new User(++USER_COUNT, "Elena", "Ivanova"));
+    }
+
+    public UserDAOImpl() {
     }
 
     @Override
     public List<User> index() {
-        return null;
+        return listUsers;
     }
 
     @Override
     public User show(int id) {
-        return null;
+        return listUsers.stream().filter(user -> user.getId() == id).findAny().orElse(null);
     }
 
     @Override
     public void save(User user) {
-
+        user.setId(++USER_COUNT);
+        listUsers.add(user);
     }
 }
---
+++ src/main/java/ru/goltsov/springCRUD/controllers/UsersController.java
@@ -0,0 +1,56 @@
+package ru.goltsov.springCRUD.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.goltsov.springCRUD.DAO.UserDAOImpl;
+import ru.goltsov.springCRUD.models.User;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+    private final UserDAOImpl userDAO;
+
+
+    @Autowired
+    public UsersController(UserDAOImpl userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("users", userDAO.index());
+        return "users/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDAO.show(id));
+
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("user", new User());
+        return "users/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userDAO.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userDAO.show(id));
+        return "users/edit";
+    }
+
+//    @PatchMapping("/{id}")
+//    public String update(ModelAttribute("user") User user,
+//    @PathVariable("id") int id, Model ){
+//    }
+}
